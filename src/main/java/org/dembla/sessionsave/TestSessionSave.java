7a5d431c41fb367811fdb6551823cd678c406f3f
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-09-19 04:02:48 +0530

    Play with Session and Transaction.

--- src/main/java/org/dembla/sessionsave/TestSessionSave.java
+++ src/main/java/org/dembla/sessionsave/TestSessionSave.java
@@ -26,8 +26,41 @@ public class TestSessionSave {
         session.flush(); //address will not get saved without this
         System.out.println("*****");
 
+
+        // Session Example With Transaction
+        Session session1 =  sessionFactory.openSession() ;
+        Emp emp1 = getTestEmployee()  ;
+        long id1 = (Long) session1.save(emp1) ;
+        System.out.println("2. Employee save called with transaction, id="+id1);
+        System.out.println("3. Before committing save transaction");
+        tx1.commit();
+        System.out.println("4. After committing save transaction");
+        System.out.println("*****");
+
+        //save example - existing row in table
+        Session session6 = sessionFactory.openSession();
+        Transaction tx6 = session6.beginTransaction();
+        Emp emp6 =  (Emp) session6.load(Emp.class, new Long(11));
+
+        //update some data
+        System.out.println("Employee Details="+emp6);
+        emp6.setName("New Name");
+        emp6.getAddress().setCity("New City");
+
+        long id6 = (Long) session6.save(emp6);
+        emp6.setName("New Name1"); // will get updated in database
+        System.out.println("5. Employee save called with transaction, id="+id6);
+        System.out.println("6. Before committing save transaction");
+        tx6.commit();
+        System.out.println("7. After committing save transaction");
+        System.out.println("*****");
+
+        // Close resources
         session.close();
+        session1.close();
+        session6.close();
         sessionFactory.close();
+
 
     }
 
